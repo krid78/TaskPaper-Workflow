a9df7c6cc71b0f7aef45c996c92f5736dfecf911
Daniel Kriesten <krid@tu-chemnitz.eu>
2014-05-07 15:58:22 +0200

    Initial Version of doing.txt
    a simple shell script to add tasks to a daly log

---
+++ doing.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# append text to doing.txt
+# add date header as project, if new day
+# by Daniel Kriesten <daniel@die-kriestens.de>
+# inspired by Jon Anhold <jon@anhold.com> 2012-04-19
+#
+# Update the DOING variable to point to the file you want to use.
+#
+
+export LANG="de_DE"
+LC_ALL="de_DE.UTF-8"
+DOING="/Users/krid/Dropbox/_Notes/30-doing.taskpaper"
+DATERES=$(date +"%a, %F;%R")
+DATE="${DATERES%;*}:"
+TIME="${DATERES#*;}"
+QUERY="{query}"
+ENTRY="- ${QUERY} @start(${TIME})"
+
+grep -q "${DATE}" ${DOING}
+RET=$?
+if [[ ${RET} -ne 0 ]]; then
+  echo ${DATE} >> ${DOING}
+fi
+
+echo -e "\t${ENTRY})" >> ${DOING}
+if [[ $? -eq 0 ]]; then
+  echo "Doing: ${ENTRY}"
+else
+  echo "FAILED!"
+fi
+
+# vim: ts=2:sw=2:tw=80:fileformat=unix
+# vim: comments& comments+=b\:# formatoptions& formatoptions+=or
+
