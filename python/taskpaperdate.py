93170505624571b883c8bce82df74363aed3cf68
Daniel Kriesten <krid@tu-chemnitz.eu>
2014-05-15 10:44:16 +0200

    remove error tags

--- python/taskpaperdate.py
+++ python/taskpaperdate.py
@@ -23,10 +23,10 @@ __FILES__ = [
     "/Users/krid/Dropbox/_Notes/00-Inbox.taskpaper",
     "/Users/krid/Dropbox/_Notes/10-Work.taskpaper",
     "/Users/krid/Dropbox/_Notes/20-Home.taskpaper",
-    "/Users/krid/Dropbox/_Notes/30-doing.taskpaper",
+#   "/Users/krid/Dropbox/_Notes/30-doing.taskpaper",
     "/Users/krid/Dropbox/_Notes/40-Studenten.taskpaper",
     "/Users/krid/Dropbox/_Notes/50-Geschenke.taskpaper",
-    "/Users/krid/Dropbox/_Notes/99-HowToOrganizeTaskPaper.taskpaper",
+#   "/Users/krid/Dropbox/_Notes/99-HowToOrganizeTaskPaper.taskpaper",
 ]
 
 __WEEKDAYS__ = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
@@ -53,6 +53,8 @@ def clear_relatives(line):
     line = line.replace(" @today", "")
     line = line.replace(" @tomorrow", "")
     line = line.replace(" @overdue", "")
+    # TODO tjis is a hack
+    line = line.replace(" @error(Unknown value for due tag)", "")
     return line
 
 def change_dates(line, thedate, today, tomorrow):
